7f5dec2594ef3c1ad583577d465ba3e15d9cc71c
Octavianus-zzw <965240130@qq.com>
2021-03-16 20:15:33 +0800

    test api

---
+++ src/main/java/com/zzwmservicetest/testeureka/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.zzwmservicetest.testeureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaZone;
+
+    @GetMapping
+    public String getConfig() {
+        return this.port + this.eurekaZone;
+    }
+}
